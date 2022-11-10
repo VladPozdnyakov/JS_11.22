e8a8bd4b53e8f2ec6388299146f38d7d56860bf8
admin <admin@MacBook-Air-admin.local>
2022-11-10 21:35:17 +0300

    did some edits

--- Homework_3/createDebounceFunction/createDebounceFunction.js
+++ Homework_3/createDebounceFunction/createDebounceFunction.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const createDebounceFunction = function(callback, delay) {
+function createDebounceFunction (callback, delay) {
     if(typeof callback !== 'function' || isNaN(delay)){
         throw new Error()
     }
     let timeout
     return function(...args) {
-        clearTimeout(timer)
+        clearTimeout(timeout)
         timeout = setTimeout(function(){
             callback.appy(this, args),
             delay
@@ -14,10 +14,10 @@ const createDebounceFunction = function(callback, delay) {
     }
 }
 
-// const log100 = () => console.log(100);
-// const debounceLog100 = createDebounceFunction(log100, 10000);
-// debounceLog100();
-// seTimeout(debounceLog100, 1000);
-// seTimeout(debounceLog100, 2000);
-// seTimeout(debounceLog100, 3000);
-// // 100 через 13с
+const log100 = () => console.log(100);
+const debounceLog100 = createDebounceFunction(log100, 10000);
+debounceLog100();
+seTimeout(debounceLog100, 1000);
+seTimeout(debounceLog100, 2000);
+seTimeout(debounceLog100, 3000);
+
