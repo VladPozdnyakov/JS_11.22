12584e96a522cf82064ca2636ff907ba1e18afa1
admin <admin@MacBook-Air-admin.local>
2022-11-15 15:15:39 +0300

    finished the task

---
+++ Homework_4/Calculator/Calculator.js
@@ -0,0 +1,51 @@
+'use strict'
+
+class Calculator {
+    constructor(par1, par2) {
+        if(!Number.isFinite(par1) || !Number.isFinite(par2) ) {
+            throw new Error('Parametras are invalid')
+        }
+        this.par1 = par1;
+        this.par2 = par2;
+        this.setX = this.setX.bind(this)
+        this.setY = this.setY.bind(this)
+        this.getSum = this.getSum.bind(this)
+        this.getMul = this.getMul.bind(this)
+        this.getSub = this.getSub.bind(this)
+        this.getDiv = this.getDiv.bind(this)
+    }
+    setX(num) {
+        if(!Number.isFinite(num)){
+            throw new Error('Parametr is invalid')
+        }
+        return this.par1 = num
+    }
+    setY(num) {
+        if(!Number.isFinite(num)){
+            throw new Error('Parametr is invalid')
+        }
+        return this.par2 = num
+    }
+    getSum() {
+        return this.par1 + this.par2
+    }
+    getMul() {
+        return this.par1 * this.par2
+    }
+    getSub() {
+        return this.par1 - this.par2
+    }
+    getDiv() {
+        if(this.par2 === 0){
+            throw new Error('Parametr two is invalid')  
+        }
+        return this.par1 / this.par2
+    }
+}
+
+
+
+
+
+
+
---
+++ Homework_4/concatStrings/concatStrings.js
@@ -0,0 +1,28 @@
+ concatStrings = (str, sep) => {
+  let returnStr = ''
+  let separator = ''
+  let check = true
+
+  if (typeof str !== 'string') {
+    check = false
+  } else {
+    returnStr = str + returnStr
+  }
+  if (typeof sep === 'string') {
+    separator = sep
+  }
+  return addStr
+
+  function addStr (nextStr) {
+    if (!arguments.length) {
+      return returnStr
+    } else if (typeof nextStr !== 'string' && check) {
+      check = false
+    } else {
+      returnStr = returnStr + separator + nextStr
+    }
+    return addStr
+  }
+}
+
+console.log(concatStrings('a')(null)('c')())
